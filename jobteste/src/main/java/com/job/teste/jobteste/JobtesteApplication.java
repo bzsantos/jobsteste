112f72b52e9d9f075583ebab84e04422dcee9c57
bzsantos <bzsantos@gmail.com>
2020-10-02 18:23:17 -0300

    Teste Project Jobs

---
+++ jobteste/src/main/java/com/job/teste/jobteste/JobtesteApplication.java
@@ -0,0 +1,13 @@
+package com.job.teste.jobteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtesteApplication.class, args);
+	}
+
+}
