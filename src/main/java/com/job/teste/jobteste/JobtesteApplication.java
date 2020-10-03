d836280d367d2b5d60157c8224c5f7560109ce22
Bruno Zolotareff <bzsantos@gmail.com>
2020-10-03 13:07:53 -0300

    Add files via upload

---
+++ src/main/java/com/job/teste/jobteste/JobtesteApplication.java
@@ -0,0 +1,15 @@
+package com.job.teste.jobteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtesteApplication.class, args);
+		
+		//teste
+	}
+
+}
---
+++ src/test/java/com/job/teste/jobteste/JobtesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.teste.jobteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
